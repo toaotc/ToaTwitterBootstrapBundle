d4ba9d516571887bd4e6e3d1a680ac4b7d9f3027
Enrico Thies <enrico.thies@gmail.com>
2012-05-17 22:05:40 +0200

    Twig extension for global variables added

---
+++ Extension/TwigGlobals.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Toa\Bundle\TwitterBootstrapBundle\Extension;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class TwigGlobals extends \Twig_Extension
+{
+    private $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+      $this->container = $container;
+    }
+
+    public function getGlobals()
+    {
+      return array('toa_twitter_bootstrap' => $this->container->getParameter('toa_twitter_bootstrap'));
+    }
+
+    public function getName()
+    {
+        return 'toa_twitter_bootstrap';
+    }
+}
